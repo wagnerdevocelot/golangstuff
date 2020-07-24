d64615fad8448ee4fa3218fdb371ddfaf3a724a4
wagner abrantes <wagner.dev1337@gmail.com>
2020-07-24 14:32:15 -0300

    poiters segunda parte

--- ponteiros/pointers.go
+++ ponteiros/pointers.go
@@ -19,8 +19,34 @@ func main() {
 	// ===> 10
 
 	// mostrando os tipos das variáveis
-	fmt.Printf("%T, %T", x, y)
+	fmt.Printf("%T, %T\n\n", x, y)
 	// ===> int, *int
 
 	// (x) é só um int, já (y) é um ponteiro que está apontando um espaço na memória que contem um int
+
+	/*
+		O uso mais comum de ponteiros é para evitar o consumo de memoria passando valores para locais diferentes
+		no programa, pelos valores em Go serem pass by value isso gera um custo alto em termos de performance.
+	*/
+
+	// duas variáveis com valores iguais
+	z := 1
+	w := 1
+
+	passByvalue(z)   // passada como valor z++ = 2
+	passBymemory(&w) // passada como ponteiro *w++ = 2
+
+	fmt.Println(z) // porem na chamada da variável z ainda é = 1 é como se a função tivesse usado um dublê
+	fmt.Println(w) // já w tem seu valor modificado diretamente na memoria
+}
+
+func passByvalue(w int) {
+	w++
+	fmt.Println("Variável passada como valor: ", w)
+
+}
+
+func passBymemory(w *int) {
+	*w++
+	fmt.Println("Variável passada como ponteiro: ", *w)
 }
