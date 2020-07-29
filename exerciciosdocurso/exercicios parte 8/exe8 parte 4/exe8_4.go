f1588d81254e646201b11d16f32148c715673b94
Wagner Abrantes <wagner.dev1337@gmail.com>
2020-07-29 16:21:02 -0300

    ordenação com sort

---
+++ exerciciosdocurso/exercicios parte 8/exe8 parte 4/exe8_4.go
@@ -0,0 +1,28 @@
+/*
+- Partindo do código abaixo, ordene a []int e a []string.
+    - https://play.golang.org/p/H_q75mpmHW
+*/
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	xi := []int{5, 8, 2, 43, 17, 987, 14, 12, 21, 1, 4, 2, 3, 93, 13}
+	xs := []string{"random", "rainbow", "delights", "in", "torpedo", "summers", "under", "gallantry", "fragmented", "moons", "across", "magenta"}
+
+	fmt.Println(xi)
+	// sort xi
+	sort.Ints(xi)
+	// ordenação de inteiros em ordem crescente
+	fmt.Println(xi)
+
+	fmt.Println(xs)
+	// sort xs
+	sort.Strings(xs)
+	// oredenação de strings em ordem alfabetica
+	fmt.Println(xs)
+}
